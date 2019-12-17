e0ce8cfa6e33f86f6a1cfca0bc9759289f0e8cc0
Kenya-a <Kenya-a@users.noreply.github.com>
2019-12-17 11:28:13 -0600

    added queue_and_stack solution from lecture

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -28,3 +28,28 @@ class Queue:
     def len(self):
         #return len(value)/returning the number of items in the queue
         return self.size
+
+"""
+Solution from Lecture:
+
+    def __init__(self):
+        self.size = 0
+        self.storage = DoublyLinkedList()
+        
+    def push(self, value):
+        #pushing to stack
+        self.size += 1
+           self.storage.add_to_head(value)
+        
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_tail()
+            #remove from the opposite in a queue
+        else:
+            return None
+
+    def len(self):
+        return self.size
+
+ """
--- queue_and_stack/dll_stack.py
+++ queue_and_stack/dll_stack.py
@@ -21,3 +21,30 @@ class Stack:
 
     def len(self):
         return self.size
+
+"""
+Solution from Lecture:
+
+    def __init__(self):
+        self.size = 0
+        self.storage = DoublyLinkedList()
+        
+    def push(self, value):
+        #pushing to stack
+        self.size += 1
+           self.storage.add_to_head(value)
+        
+    def pop(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_from_head()
+        else:
+            return None
+
+    def len(self):
+        return self.size
+
+ """
+
+
+
