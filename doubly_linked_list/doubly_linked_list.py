66542b26ee30ba0a33cd7500364d7bc4acb90819
Kenya-a <Kenya-a@users.noreply.github.com>
2019-12-17 09:42:36 -0600

    move_to_front - test passed ///move_to_end - test failed

--- doubly_linked_list/doubly_linked_list.py
+++ doubly_linked_list/doubly_linked_list.py
@@ -73,7 +73,7 @@ class DoublyLinkedList:
         value = self.head.value
         self.delete(self.head)
         return value
-        
+
     """Wraps the given value in a ListNode and inserts it 
     as the new tail of the list. Don't forget to handle 
     the old tail node's next pointer accordingly."""
@@ -101,13 +101,20 @@ class DoublyLinkedList:
     """Removes the input node from its current spot in the 
     List and inserts it as the new head node of the List."""
     def move_to_front(self, node):
-        pass
+        if node is self.head:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_head(value)
 
     """Removes the input node from its current spot in the 
     List and inserts it as the new tail node of the List."""
     def move_to_end(self, node):
-        pass
-
+        if node is self.tail:
+            return
+        value = node.value
+        self.delete(node)
+        self.add_to_tail(value)
     """Removes a node from the list and handles cases where
     the node was the head or the tail"""
     def delete(self, node):
