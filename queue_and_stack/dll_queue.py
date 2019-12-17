d94c8319703eb75fc58cfe1533110f3319429d07
Kenya-a <Kenya-a@users.noreply.github.com>
2019-12-17 10:28:06 -0600

    dll_queue.py -test passed

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -6,14 +6,25 @@ from doubly_linked_list import DoublyLinkedList
 class Queue:
     def __init__(self):
         self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        # Why is our DLL a good choice to store our elements? Serialize data coming in from multiple sources/ can be travesed in both forward and backward direction/ more efficient
+        self.storage = []
 
+
+    """add(an item of data awaiting processing) to a queue of such items"""
     def enqueue(self, value):
-        pass
+        # value.insert(0, value)
+        self.storage.insert(0, value)
+        self.size += 1 #size of the queue increases
 
+    """remove(and item of data awaiting processing) from a queue of such items"""
     def dequeue(self):
-        pass
+        #return self.value.pop()
+        if self.size == 0:
+            return 
+        else:
+            self.size -= 1
+            return self.storage.pop()
 
     def len(self):
-        pass
+        #return len(value)/returning the number of items in the queue
+        return self.size
